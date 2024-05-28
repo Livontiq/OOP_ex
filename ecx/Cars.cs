d3559e981dce0211f0d9b179fb2effdd4afccfc3
10201470 <10201470@B206PC26>
2024-05-28 20:48:22 +0300

    Добавьте файлы проекта.

---
+++ ecx/Cars.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ecx
+{
+    public partial class Cars
+    {
+        public int id { get; set; }
+        public string carNumber { get; set; }
+        public string carType { get; set; }
+        public string carCost { get; set; }
+        public string carStatus { get; set; }
+    }
+}
